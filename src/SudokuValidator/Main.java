786c16644ce82c07f9f7e635b13368b58263eb2f
Andy De Gheldere <andy.degheldere@gmail.com>
2022-04-24 13:40:03 +0200

    updated solution

--- src/SudokuValidator/Main.java
+++ src/SudokuValidator/Main.java
@@ -28,14 +28,14 @@ public class Main {
                 {6, 7, 2, 1, 9, 5, 3, 4, 8},
                 {1, 9, 8, 3, 4, 2, 5, 6, 7},
                 {8, 5, 9, 7, 6, 1, 4, 2, 3},
-                {4, 2, 6, 8, 5, 3, 2, 9, 1},
+                {4, 2, 6, 8, 5, 3, 7, 9, 1},
                 {7, 1, 3, 9, 2, 4, 8, 5, 6},
-                {9, 6, 1, 5, 3, 7, 7, 8, 4},
+                {9, 6, 1, 5, 3, 7, 2, 8, 4},
                 {2, 8, 7, 4, 1, 9, 6, 3, 5},
                 {3, 4, 5, 2, 8, 6, 1, 7, 9}
         };
 
-       boolean isValidSudoku = SudokuSolutionValidator.check(sudoku);
-       System.out.printf("The sudoku solution is %s", isValidSudoku ? "valid" : "invalid");
+        boolean isValidSudoku = SudokuSolutionValidator.check(sudoku);
+        System.out.printf("The sudoku solution is %s", isValidSudoku ? "valid" : "invalid");
     }
 }
--- src/SudokuValidator/SudokuSolutionValidator.java
+++ src/SudokuValidator/SudokuSolutionValidator.java
@@ -8,13 +8,6 @@ public class SudokuSolutionValidator {
         //  All the rows/columns/3x3-arrays should contain the same elements as this one.
         int[] required = IntStream.rangeClosed(1, 9).toArray();
 
-        //  Check all rows
-        for (int row = 0; row < 9; row++) {
-            boolean isEqual = compareArrays(sudoku[row], required);
-            System.out.printf("Row %d meets requirements: %b\n", row+1, isEqual);
-            if (!isEqual) return false;
-        }
-
         //  Transpose array
         int[][] sudokuTranspose = new int[9][9];
         for (int row = 0; row < 9; row++) {
@@ -23,17 +16,16 @@ public class SudokuSolutionValidator {
             }
         }
 
-        //  Check all original columns
+        //  Check all rows & columns
         for (int row = 0; row < 9; row++) {
-            boolean isEqual = compareArrays(sudokuTranspose[row], required);
-            System.out.printf("Column %d meets requirements: %b\n", row+1, isEqual);
+            boolean isEqual = compareArrays(sudoku[row], sudokuTranspose[row]);
             if (!isEqual) return false;
         }
 
         //  Check all the 3x3-arrays
-        for (int i = 0; i < 9; i+=3) {
-            for (int j = 0; j < 9; j+=3) {
-                int[][] threeByThree = sub2DArray(sudoku, i, i+3, j, j+3);
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                int[][] threeByThree = sub2DArray(sudoku, i, i + 3, j, j + 3);
                 int[] flattenedThree = Arrays.stream(threeByThree).flatMapToInt(Arrays::stream).sorted().toArray();
                 boolean is3x3Equal = (compareArrays(flattenedThree, required));
                 if (!is3x3Equal) return false;
@@ -45,17 +37,19 @@ public class SudokuSolutionValidator {
 
     //  Check if arrays are equal
     public static boolean compareArrays(int[] arr1, int[] arr2) {
-        int[] copyOne = Arrays.copyOf(arr1,arr1.length);
-        Arrays.sort(copyOne);
-        return Arrays.equals(copyOne, arr2);
+        int[] copy1 = Arrays.copyOf(arr1, arr1.length);
+        int[] copy2 = Arrays.copyOf(arr2, arr2.length);
+        Arrays.sort(copy1);
+        Arrays.sort(copy2);
+        return Arrays.equals(copy1, copy2);
     }
 
     //  Create 2D-subarray
-    public static int[][] sub2DArray (int[][] srcArray, int row_begin, int row_end
-                                , int col_begin, int col_end) {
+    public static int[][] sub2DArray(int[][] srcArray, int row_begin, int row_end
+            , int col_begin, int col_end) {
         int[][] subArray = new int[row_end - row_begin][col_end - col_begin];
-        for (int r = row_begin, i=0; r < row_end; r++, i++){
-            for (int c = col_begin, j= 0; c < col_end; c++, j++){
+        for (int r = row_begin, i = 0; r < row_end; r++, i++) {
+            for (int c = col_begin, j = 0; c < col_end; c++, j++) {
                 subArray[i][j] = srcArray[r][c];
             }
         }
@@ -63,7 +57,7 @@ public class SudokuSolutionValidator {
     }
 
     /*
-  2D-Array print utility
+    2D-Array print utility
     public static void print2DArray(int[][] arr){
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
